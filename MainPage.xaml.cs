3df93f1bd55a76c260d981e786d3b11abe3b25cc
derek <derek@SCARLETTE>
2023-03-02 09:46:16 -0500

    Add project files.

---
+++ MainPage.xaml.cs
@@ -0,0 +1,31 @@
+﻿namespace MauiApp6;
+
+/* MVVM Pattern
+Model : Represents information about a real world object
+View : The user interface and its components
+ViewModel : Connects the model and View
+*/
+
+
+public partial class MainPage : ContentPage
+{
+	int count = 0;
+
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+
+	private void OnCounterClicked(object sender, EventArgs e)
+	{
+		count++;
+
+		if (count == 1)
+			CounterBtn.Text = $"Clicked {count} time";
+		else
+			CounterBtn.Text = $"Clicked {count} times";
+
+		SemanticScreenReader.Announce(CounterBtn.Text);
+	}
+}
+
